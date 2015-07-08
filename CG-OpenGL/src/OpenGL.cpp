be4673add9e77e1035be9e42463598f3ae75114f
Lucas Petry <lucasmaypetry@gmail.com>
2015-07-08 01:34:48 -0700

    Estrutura básica do arquivo montada.

--- CG-OpenGL/src/OpenGL.cpp
+++ CG-OpenGL/src/OpenGL.cpp
@@ -1,237 +1,123 @@
-/*
- *		Este código foi modificado e extendido por Aldo v.Wangenheim
- *		(awangenh@inf.ufsc.br - http://www.inf.ufsc.br/~awangenh/CG/)
- *		para utilização na:
- *		Disciplina ine 5341 - Computacao Grafica do Departamento de
- *		Informatica e Estatistica da Universidade Federal de Santa
- *		Catarina.
- *
- *		Florianopolis, Ilha de Santa Catarina, Maio/Junho de 2001
- *
- *
- *		This Code Was Originally Created By Jeff Molofee 2000
- *		A HUGE Thanks To Fredric Echols For Cleaning Up
- *		And Optimizing The Base Code, Making It More Flexible!
- *		If You've Found This Code Useful, Please Let Me Know.
- *		Visit My Site At nehe.gamedev.net
- *
- *		Parts of this code were Written by Michael Sweet.
- *		The OpenGL SuperBible
- *
+#ifndef OPENGL_H_
+#define OPENGL_H_
+
+#include <GL/glut.h>
+
+/*===================== Teclas =====================*/
+const unsigned char TECLA_ESC = 27;
+const unsigned char TECLA_CIMA = GLUT_KEY_UP;
+const unsigned char TECLA_BAIXO = GLUT_KEY_DOWN;
+/*==================================================*/
+
+/*============== Atributos da Janela ===============*/
+int janela;
+unsigned int larguraJanela;
+unsigned int alturaJanela;
+/*==================================================*/
+
+/**
+ * Inicializar configurações do OpenGL.
  */
-
-#include <stdlib.h>		// incluimos stdlib para compatibilidade com
-						// microsoft windows. Em Unix esta linha eh
-						// sem efeito porque stdlib eh incluido por
-						// default sempre. Se voce esquecer esta linha
-						// em windows, seu compilador vai dizer que
-						// nao conhece a funcao exit()...
-
-#include <stdio.h>		// Incluimos stdio para podermos usar printf()
-						// para debugar alguma coisa que queiramos.
-						// Mais tarde vamos usar fscanf e fgets para
-						// ler dados. Se voce esta usando uma versao
-						// antiga de Borland C++ (ou Turbo C) ou mesmo
-						// outro compilador C p/windows fora de padrao
-						// ANSI, voce provavelmente tera de incluir
-						// um headerfile obsoleto chamado conio.h aqui
-
-#include <GL/glut.h>	// Aqui sao incluidas TODAS as definicoes
-						// para tanto o OpenGL como o GLUT compilarem com
-						// sucesso em QUALQUER sistema operacional.
-						// NUNCA inclua windows.h ou outro headerfile
-						// especifico a windows em seus programas, que
-						// voce estara produzindo codigo que so compila
-						// nesta plataforma. O glut.h resolve a independencia
-						// de plataforma de todo codigo OpenGL de uma forma
-						// extremamente elegante: a versao de glut.h para
-						// cada plataforma inclue os headerfiles especificos
-						// para aquela plataforma.
-
-#define ESCAPE 27		/* ascii code for the escape key */
-
-
-
-/*
- * Globals...
- */
-
-
-int		window;				/* The number of our GLUT window */
-
-int		Width;				/* Width of window */
-int		Height;				/* Height of window */
-
-
-/*
- * Functions...
- */
-
-
-void InitGL(int Width, int Height)	        // We call this right after our OpenGL window is created.
-{
-  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);		// This Will Clear The Background Color To Black
-  glClearDepth(1.0);				// Enables Clearing Of The Depth Buffer
-  glDepthFunc(GL_LEQUAL);			// The Type Of Depth Test To Do
-  glEnable(GL_DEPTH_TEST);			// Enables Depth Testing
-  glShadeModel(GL_SMOOTH);			// Enables Smooth Color Shading
-
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();					// Reset The Projection Matrix
-
-  glMatrixMode(GL_MODELVIEW);
+void inicializarOpenGL() {
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);	// Limpar a janela para preto
+	glClearDepth(1.0);						// Habilitar limpeza do buffer de profundidade
+	glDepthFunc(GL_LEQUAL);					// Tipo do teste de profundidade
+	glEnable(GL_DEPTH_TEST);				// Habilitar teste de profundidade
+	glShadeModel(GL_SMOOTH);				// Habilitar Smooth Color Shading
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();						// Carregar a matriz identidade como inicial
+	glMatrixMode(GL_MODELVIEW);
 }
 
-/*==========================================================*/
-/*	The function called whenever a key is pressed.			*/
-void
-tecla(unsigned char tecla, int x, int y)
-/*==========================================================*/
-{
-  switch ( tecla ) {
-    case 27:			// When Escape Is Pressed...
-		/* shut down our window */
-		glutDestroyWindow(window);
-		/* exit the program...normal termination. */
-		exit(0);
-		break;			// Ready For Next Case
-    default:			// Now Wrap It Up
-		break;
-  }
+/**
+ * Redesenhar o mundo.
+ */
+void redesenharMundo() {
+	// Limpar a janela
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
 
-}
+	/**
+	 * DESENHAR ALGO
+	 */
 
-
-/*==========================================================*/
-/*	The function called whenever a special key is pressed.	*/
-void
-teclas_de_seta ( int tecla, int x, int y )
-/*==========================================================*/
-{
-  switch ( tecla ) {
-    case GLUT_KEY_UP:					// When Up Arrow Is Pressed...
-      glutFullScreen ( );				// Go Into Full Screen Mode
-      break;
-    case GLUT_KEY_DOWN:					// When Down Arrow Is Pressed...
-      glutReshapeWindow ( 640, 480 );	// Go Into A 640 By 480 Window
-      break;
-    default:
-		printf("Teclaram: %d\n", tecla);
-      break;
-  }
-}
-
-
-
-/*==========================================================*/
-/*  redesenhaMundo() - Redraw the window...					*/
-void
-redesenhaMundo(void)
-/*==========================================================*/
-    {
-    /* Clear the window to black */
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
-
-	/* eh AQUI que você vai chamar todas as funcoes que
-	   desenham algo */
-
+	// Trocar o buffer da memória para ser desenhado
 	glutSwapBuffers();
+}
 
-    }
+/**
+ * Mostrar mundo (Idle function - quando nada está sendo feito).
+ */
+void mostrarMundo() {
+	redesenharMundo();
+}
 
+/**
+ * Redimensionar a janela.
+ */
+void redimensionarJanela(int largura, int altura) {
+    larguraJanela  = largura;
+    alturaJanela = altura;
 
-
-/*=====================================================	*/
-/* redimensionaJanela() - Resize the window...			*/
-void
-redimensionaJanela(	int width,  /* I - Width of window	*/
-					int height) /* I - Height of window	*/
-/*=====================================================	*/
-    {
-    /* Save the new width and height */
-    Width  = width;
-    Height = height;
-
-    /* Reset the viewport... */
-    glViewport(0, 0, width, height);
+    // Reiniciar a Viewport
+    glViewport(0, 0, larguraJanela, alturaJanela);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
     glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity(); // Reset The Modelview Matrix
-
-    }
-
-
-/*===========================================================*/
-/* Funcao que chama redraw.									 */
-/* Endereco passado como a Idle Function, funcao que		 */
-/* o laco do GLUT chama quando nao esta fazendo outra coisa. */
-void
-mostraMundo(void)
-/*===========================================================*/
-{
-
-	redesenhaMundo();
-
+	glLoadIdentity();
 }
 
-/*==========================================================*/
-/*	'main()' - Open a window and display some text.			*/
-int                /* O - Exit status						*/
-main(int  argc,    /* I - Number of command-line arguments	*/
-     char *argv[]) /* I - Command-line arguments			*/
-/*==========================================================*/
-    {
-	/* Initialize GLUT state - glut will take any command line arguments that pertain to it or
-     X Windows - look at its documentation at http://reality.sgi.com/mjk/spec3/spec3.html */
-    glutInit(&argc, argv);
-	/*	Select type of Display mode:
-		Double buffer
-		RGBA color
-		Alpha components supported
-		Depth buffer */
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+/**
+ * Tecla comum pressionada.
+ */
+void tecla(unsigned char tecla, int x, int y) {
+	switch(tecla) {
+		case TECLA_ESC:
+			glutDestroyWindow(janela);
+			exit(0);
+			break;
+		default:
+			break;
+	}
+}
 
-	/* get a 640 x 480 window */
-    glutInitWindowSize(640, 480);
+/**
+ * Tecla especial pressionada.
+ */
+void teclas_de_seta( int tecla, int x, int y ) {
+	switch(tecla) {
+		case TECLA_CIMA:
+			glutFullScreen();
+			break;
+		case TECLA_BAIXO:
+			glutReshapeWindow(640, 480);
+			break;
+		default:
+			//printf("Teclaram: %d\n", tecla);
+			break;
+	}
+}
 
-	/*	The window starts at the upper left corner of the screen	*/
-	/*	Aparentemente esta funcao so faz sentido no X-Windows. Em	*/
-	/*	MS-Windows a janela aparece sempre aa esquerda em cima de	*/
-	/*	qualquer forma.												*/
+/**
+ * Função principal do programa.
+ */
+int main(int argc, char *argv[]) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(640, 480);
 	glutInitWindowPosition(0, 0);
-
-	/* Abrimos aqui uma janela, Open a window						*/
-	/* A variavel window guarda o numero da janela. Pode ser util.	*/
-    window = glutCreateWindow("ine5341 Computação Gráfica UFSC - Rotação de Objetos Facetados e Modelos de Arame");
-
-    /*	Register the function called when our window is resized.	*/
-	glutReshapeFunc(redimensionaJanela);
-
-	/*	Register the function to do all our OpenGL drawing.			*/
-    glutDisplayFunc(redesenhaMundo);
-
-	/* Register the function called when the keyboard is pressed.	*/
+	janela = glutCreateWindow("Título da Janela");
+	glutReshapeFunc(redimensionarJanela);
+	glutDisplayFunc(redesenharMundo);
 	glutKeyboardFunc(tecla);
-
-	/* Resgitramos a funcao que é chamada quando uma tecla especial */
-	/* foi teclada, como setas e teclas de funcao.					*/
 	glutSpecialFunc(teclas_de_seta);
-
-
-	/* Funcao chamada sempre que o Loop do GLUT esta ocioso			*/
-	/* Esta funcao eh responsavel pelos efeitos de animacao.		*/
-	glutIdleFunc(mostraMundo);
-
-	/* Chamamos a funcao que realiza o resto das inicializacoes.	*/
-	InitGL( 640, 480);
-
-    /* Iniciamos a maquina de processamento de eventos do GLUT.		*/
+	glutIdleFunc(mostrarMundo);
+	inicializarOpenGL();
 	glutMainLoop();
-    return (0);
-    }
+	return 0;
+}
 
+#endif /* OPENGL_H_ */
