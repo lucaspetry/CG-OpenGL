1f3f36b9a85bdbf91b03f7f4a9e67da5a8593fcc
Lucas Petry <lucasmaypetry@gmail.com>
2015-07-13 18:50:23 -0700

    Estágio adicionado à caminhada.

--- CG-OpenGL/include/Cavalo.h
+++ CG-OpenGL/include/Cavalo.h
@@ -8,7 +8,7 @@ class Cavalo {
 
 public:
 	static const int NUMERO_MEMBROS = 16;
-	static const int ESTAGIOS_CAMINHADA = 6;
+	static const int ESTAGIOS_CAMINHADA = 7;
 	static const int ESTAGIOS_TROTE = 4;
 	static const int ESCALA = 1;
 
@@ -70,26 +70,26 @@ private:
 	 * Angulos de caminhada. Cada ângulo é relativo ao membro anteriormente desenhado.
 	 */
 	const double angulosCaminhada[NUMERO_MEMBROS][ESTAGIOS_CAMINHADA] = {
-			{ 0, 0, 0, 0, 0, 0 }, /* CABEÇA */
-			{ 0, 0, 0, 0, 0, 0 }, /* PESCOÇO */
-			{ 0, 0, 0, 0, 0, 0 }, /* TRONCO */
+			{ 0, 0, 0, 0, 0, 0, 0 }, /* CABEÇA */
+			{ 0, 0, 0, 0, 0, 0, 0 }, /* PESCOÇO */
+			{ 0, 0, 0, 0, 0, 0, 0 }, /* TRONCO */
 
-			{ 2, -5, -16, -3, 33, 40 }, /* ANTEBRAÇO FRONTAL DIREITO */
-			{ -5, 27, 35, 6, -2, -14 }, /* ANTEBRAÇO FRONTAL ESQUERDO */
-			{ -27, -35, -33, -10, -5, -16 }, /* ANTEBRAÇO TRASEIRO DIREITO */
-			{ -1, -11, -21, -30, -40, -35 }, /* ANTEBRAÇO TRASEIRO ESQUERDO */
+			{ 2, -5, -16, -3, 15, 33, 40 }, /* ANTEBRAÇO FRONTAL DIREITO */
+			{ -5, 27, 35, 6, 2, -2, -14 }, /* ANTEBRAÇO FRONTAL ESQUERDO */
+			{ -27, -35, -33, -10, -7.5, -5, -16 }, /* ANTEBRAÇO TRASEIRO DIREITO */
+			{ -1, -11, -21, -30, -35, -40, -35 }, /* ANTEBRAÇO TRASEIRO ESQUERDO */
 
-			{ 3, 0, 0, -30, -68, -40 }, /* CANELA FRONTAL DIREITA */
-			{ -27, -76, -34, 1, 0, 1 }, /* CANELA FRONTAL ESQUERDA */
-			{ 38, 41, 55, 59, 33, 27 }, /* CANELA TRASEIRA DIREITA */
-			{ 47, 40, 33, 35, 43, 80 }, /* CANELA TRASEIRA ESQUERDA */
+			{ 3, 0, 0, -30, -49, -68, -40 }, /* CANELA FRONTAL DIREITA */
+			{ -27, -76, -34, 1, 0.5, 0, 1 }, /* CANELA FRONTAL ESQUERDA */
+			{ 38, 41, 55, 59, 46, 33, 27 }, /* CANELA TRASEIRA DIREITA */
+			{ 47, 40, 33, 35, 39, 43, 80 }, /* CANELA TRASEIRA ESQUERDA */
 
-			{ 32, 42, 35, -3, -23, -3 }, /* CASCO FRONTAL DIREITO */
-			{ -3, -3, 1, 32, 38, 41 }, /* CASCO FRONTAL ESQUERDO */
-			{ 35, -35, -59, -54, 21, 38 }, /* CASCO TRASEIRO DIREITO */
-			{ -46, 13, 0, 28, -30, -80 }, /* CASCO TRASEIRO ESQUERDO */
+			{ 32, 42, 35, -3, -13, -23, -3 }, /* CASCO FRONTAL DIREITO */
+			{ -3, -3, 1, 32, 35, 38, 41 }, /* CASCO FRONTAL ESQUERDO */
+			{ 35, -35, -59, -54, -18, 21, 38 }, /* CASCO TRASEIRO DIREITO */
+			{ -46, 13, 0, 28, -1, -30, -80 }, /* CASCO TRASEIRO ESQUERDO */
 
-			{ 0, 0, 0, 0, 0, 0 } /* RABO */
+			{ 0, 0, 0, 0, 0, 0, 0 } /* RABO */
 	};
 
 	/**
